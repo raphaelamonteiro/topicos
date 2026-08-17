6e649f653ca6d4f8c6cbf011a49a289d02ac8319
Raphaela <bmraphaela@gmail.com>
2026-08-17 19:04:48 -0300

    init: iniciando projeto com spring boot

---
+++ src/main/java/br/com/topicos/TopicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.topicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicosApplication.class, args);
+	}
+
+}
